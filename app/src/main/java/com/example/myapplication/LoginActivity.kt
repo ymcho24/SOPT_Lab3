5df81577fe00631d2123871bfe5675be760c7a3a
kangheewon <ymcho24@naver.com>
2020-05-20 20:34:36 +0900

    기본과제1

--- app/src/main/java/com/example/myapplication/LoginActivity.kt
+++ app/src/main/java/com/example/myapplication/LoginActivity.kt
@@ -8,9 +8,16 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import com.example.myapplication.data.RequestLogin
+import com.example.myapplication.data.ResponseLogin
+import com.example.myapplication.network.RequestToServer
 import kotlinx.android.synthetic.main.activity_login.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class LoginActivity : AppCompatActivity() {
+    val requestToServer = RequestToServer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,8 +27,35 @@ class LoginActivity : AppCompatActivity() {
             if(et_id.text.isNullOrBlank()||et_pwd.text.isNullOrBlank()){
                 Toast.makeText(this,"아이디와 비밀번호를 확인하세요", Toast.LENGTH_SHORT).show()
             }else{
-                val intent = Intent(this,MainActivity::class.java)
-                startActivity(intent)
+                requestToServer.service.requestLogin(
+                    RequestLogin(
+                        id = et_id.text.toString(),
+                        password = et_pwd.text.toString()
+                    )
+                ).enqueue(object :Callback<ResponseLogin>{
+                    override fun onFailure(call: Call<ResponseLogin>, t: Throwable) {
+                        //통신 실패
+                    }
+
+                    override fun onResponse(
+                        call: Call<ResponseLogin>,
+                        response: Response<ResponseLogin>
+                    ) {
+                        //통신 성공
+                        if(response.isSuccessful) {
+                            if(response.body()!!.success){
+                                Toast.makeText(this@LoginActivity, "로그인 성공", Toast.LENGTH_SHORT).show()
+                                val intent = Intent(this@LoginActivity,MainActivity::class.java)
+                                startActivity(intent)
+                                finish()
+                            }else{
+                                Toast.makeText(this@LoginActivity, "아이디/비밀번호를 확인하세요!", Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                    }
+
+                })
+
             }
         }
         tv_register.setOnClickListener{
